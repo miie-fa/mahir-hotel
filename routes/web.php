cd017cbf549c65fbb004cadcf2690e52f4cac60c
Dina afriyani <dinaafriyani801@gmail.com>
2024-06-12 21:11:34 +0700

    apa aja

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/page', [HomeController::class,'index'])->name('landing');
+Route::get('/regis', [AuthController::class,'index'])->name('user.register');
+Route::get('/login',[AuthController::class,'login'])->name('user.login');
