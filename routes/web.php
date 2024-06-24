89b38570ab514cfe96434ce02e631ffe5c032123
ellusionary24 <immanuelpetrusresidul@gmail.com>
2024-06-24 15:48:17 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\RegisController;
 
-Route::get('/page', [HomeController::class,'index'])->name('user.landing');
-Route::get('/regis', [AuthController::class,'index'])->name('user.register');
-Route::get('/login',[AuthController::class,'login'])->name('user.login');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
