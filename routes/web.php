13c8e780c27e740eec92dc6c9947d7699ec99eff
Noelfyn <witsarr@gmail.com>
2024-06-29 00:37:08 +0700

    Booking Form + Pop Up.
    Data belum bisa tersimpan dalam database.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/booking', [BookingController::class,'main'])->name('booking.main');
+Route::post('/booking/process', [BookingController::class,'store'])->name('booking.store');
+Route::get('/booking/show', [BookingController::class,'show'])->name('booking.show');
+Route::get('/booking/delete', [BookingController::class,'destroy'])->name('batalkan.pemesanan');
+
